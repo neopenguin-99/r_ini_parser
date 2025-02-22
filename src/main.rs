bc1a53bf3d1096799a811eb2febaf9d31e05bdb8
Thomas Walker <codemonkeweb@proton.me>
2025-02-22 23:58:52 +0000

    added more code to the ini file parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::{self, read};
 
 use clap::{arg, crate_authors, crate_version, value_parser, Arg, ArgAction, ArgMatches, Command, ValueEnum};
+use std::collections::HashMap;
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut matches: ArgMatches = Command::new("parser")
@@ -14,7 +16,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         Some(x) => x,
         None => panic!("please provide a file name")
     };
-    let file_contents = fs::read_to_string(file_name)?;
-    println!("file_contents: {}", file_contents);
+    let file_contents = fs::read_to_string(&file_name)?;
+    let file_contents_split = file_name.split('\n').collect::<Vec<_>>();
+    println!("file_contents: {:#?}", file_contents);
     Ok(())
+}
+
+fn parse(file_contents: &Vec<&str>) -> Section {
+    let section_to_add: Section;
+    for file_contents_line in file_contents {
+        if file_contents_line.contains('=') {
+
+        }
+        else if file_contents_line.starts_with('[') && file_contents_line.ends_with(']') {
+            let section_name = &file_contents_line[0..file_contents_line.len() - 1];
+            section_to_add.sub_sections.push(Section::new(parse(file_contents)))
+        }
+    }
+    Section::new(String::from("main"), Vec::new(), HashMap::new())
+}
+
+struct Section {
+    section_name: String,
+    sub_sections: Vec<Box<Section>>,
+    key_value_pair_hashmap: HashMap<String, String>
+}
+impl Section {
+    fn new(section_name: String, sub_section: Vec<Box<Section>>, key_value_pair_hashmap: HashMap<String, String>) -> Section {
+        Section {
+            section_name,
+            sub_section,
+            key_value_pair_hashmap
+        }
+    }
 }
